4ec6530b8dbe2825e1f922a7a7233573110b7bea
yayikayo <umamicorpse@gmail.com>
2025-05-16 00:27:40 -0400

    Update sign-up, login, and logout

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,12 @@
 import LoginPanel from "./components/Login/Login"
+import Register from './components/Register/Register'
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
