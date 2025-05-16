b018d522b9c9aad2d31510faa76c529a1b271091
yayikayo <umamicorpse@gmail.com>
2025-05-16 05:10:13 -0400

    Update car model and make

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://muhammadaaq1-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://muhammadaaq1-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://muhammadaaq1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://muhammadaaq1-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://muhammadaaq1-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://muhammadaaq1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
