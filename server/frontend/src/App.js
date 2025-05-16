eb7a8972acf8c28ac2574558dedfcf3619cd8a11
yayikayo <umamicorpse@gmail.com>
2025-05-16 06:02:26 -0400

    Update dynamic pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,5 +1,8 @@
 import LoginPanel from "./components/Login/Login"
 import Register from './components/Register/Register'
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 import { Routes, Route } from "react-router-dom";
 
@@ -8,6 +11,9 @@ function App() {
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
